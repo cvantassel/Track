65f04d4947902667482a89005d3dc107acc529f9
Caleb Van Tassel <calebvantassel@gmail.com>
2020-01-11 20:49:04 -0600

    Added Homepage elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Meta } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
@@ -13,8 +15,10 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private meta: Meta
   ) {
+    this.meta.addTag({ name: 'theme-color', content: '#504D55' });
     this.initializeApp();
   }
 
